16fd4a92bf8f89b619f461dc1a34428dceab4b11
amansinghnishad <amansinghnishad0808@gmail.com>
2024-05-13 22:36:35 +0530

    Hero-section

---
+++ src/Components/HeroPage/HeroPage.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+
+const HeroPage = () => {
+  const tags = React.useMemo(
+    () => [
+      "a Developer.",
+      "a Designer.",
+      "a Programmer.",
+      "a Learner.",
+      "a Enthusiast.",
+    ],
+    []
+  );
+  const [currentTag, setCurrentTag] = useState(tags[0]);
+  const [tagIndex, setTagIndex] = useState(0);
+  const [charIndex, setCharIndex] = useState(0);
+
+  useEffect(() => {
+    const tag = tags[tagIndex];
+    if (charIndex < tag.length) {
+      setTimeout(() => {
+        setCurrentTag((prev) => prev + tag[charIndex]);
+        setCharIndex((prev) => prev + 1);
+      }, 150);
+    } else {
+      setTimeout(() => {
+        setTagIndex((prev) => (prev + 1) % tags.length);
+        setCharIndex(0);
+        setCurrentTag("");
+      }, 2000);
+    }
+  }, [tags, tagIndex, charIndex]);
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen w-full">
+      <div className="text-center">
+        <h2 className="text-3xl tracking-wide font-serif p-2">Hi there !!!</h2>
+        <h1 className="text-4xl tracking-wide font-serif p-2">
+          I am Aman Singh Nishad.
+        </h1>
+        <h3 className="text-2xl tracking-wide font-serif">
+          I am {currentTag}
+          <span className="animate-blink">|</span>
+        </h3>
+      </div>
+      <div className="fixed bottom-5 w-full text-2xl text-center">
+        <h1>Scroll</h1>
+      </div>
+    </div>
+  );
+};
+
+export default HeroPage;
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    letterSpacing: {
+      tightest: "-.075em",
+      tighter: "-.05em",
+      tight: "-.025em",
+      normal: "0",
+      wide: ".025em",
+      wider: ".05em",
+      widest: ".25em",
+    },
+    extend: {
+      animation: {
+        blink: "blink 1s infinite",
+      },
+      keyframes: {
+        blink: {
+          "0%": { opacity: "1" },
+          "50%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
