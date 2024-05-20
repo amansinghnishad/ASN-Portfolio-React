a3c3f42473483b5589e9ee6206277c7ed5ef9bc6
amansinghnishad <amansinghnishad0808@gmail.com>
2024-05-20 18:10:31 +0530

    project Card styling

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,33 +1,55 @@
-import style from "./Projects.module.css";
 import Contact from "../Contact/Contact";
+import { Card } from "./Card";
+import { BrowserRouter as Router } from "react-router-dom";
+import CorsaleImageGallery from "../../assets/CorsaleImageGallery.png";
 
 const Project = () => {
   const projects = [
-    { img: "img1", about: "about1", bgImg: "card-hello-background.png" },
-    { img: "img2", about: "about2", bgImg: "card-hello-background.png" },
-    { img: "img3", about: "about3", bgImg: "card-hello-background.png" },
-    { img: "img4", about: "about4", bgImg: "card-hello-background.png" },
-    { img: "img5", about: "about5", bgImg: "card-hello-background.png" },
+    {
+      img: CorsaleImageGallery,
+      heading: "Corsale Image Gallery",
+      about:
+        "Corsale Image Gallery is a simple and lightweight JavaScript-based project that allows users to create an interactive image gallery",
+      TryNowlink: "https://amansinghnishad.github.io/Corsale-Image-Gallery/",
+      codeLink: "https://github.com/amansinghnishad/Corsale-Image-Gallery",
+    },
+    {
+      img: "img2",
+      heading: "heading 1",
+      about: "about2",
+      TryNowlink: "",
+      codeLink: "",
+    },
+    {
+      img: "img3",
+      heading: "heading 1",
+      about: "about3",
+      TryNowlink: "",
+      codeLink: "",
+    },
+    {
+      img: "img4",
+      heading: "heading 1",
+      about: "about4",
+      TryNowlink: "",
+      codeLink: "",
+    },
+    {
+      img: "img5",
+      heading: "heading 1",
+      about: "about5",
+      TryNowlink: "",
+      codeLink: "",
+    },
   ];
   return (
     <>
       <div className=" w-full h-auto flex justify-center flex-wrap p-5">
-        {projects.map((project, index) => (
-          <div
-            key={index}
-            className={`w-2/5 h-96 m-5 rounded-3xl relative overflow-hidden shadow-xl shadow-slate-500 bg-slate-900 ${style.projectDiv}`}
-          >
-            <img
-              src={project.bgImg}
-              className="absolute left-1/2 top-1/2 pointer-events-none -translate-x-1/2 -translate-y-1/2 h-full w-full z-10 rounded-3xl"
-            />
-            <div className="text-white flex justify-center">{project.img}</div>
-
-            <div className="text-white flex justify-center">
-              {project.about}
-            </div>
-          </div>
-        ))}
+        <Router>
+          {projects.map((project, index) => (
+            <Card key={index} project={project} />
+          ))}
+        </Router>
       </div>
       <Contact />
     </>
