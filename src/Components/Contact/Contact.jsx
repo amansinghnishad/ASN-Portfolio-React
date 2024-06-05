40457e0035ddaf76806c055b001acd0202feeb91
amansinghnishad <amansinghnishad0808@gmail.com>
2024-06-05 19:08:19 +0530

    footer and Contact-component

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,8 +1,14 @@
+import Footer from "../Footer/Footer";
+
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact</h1>
-    </div>
+    <>
+      <div className="p-5 grid grid-cols-2 w-full h-40 border-white border-2 justify-center items-center ">
+        <div className="m-2 border-yellow-500 border-2"> 1 </div>
+        <div className="m-2 border-red-400 border-2"> 2 </div>
+      </div>
+      <Footer />
+    </>
   );
 };
 
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="text-white flex justify-center items-center">
+        <p>Made with ❣️ by ASN!</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
