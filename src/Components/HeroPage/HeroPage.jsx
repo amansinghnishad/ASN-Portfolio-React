6b6d72f4632f4ca22169111beff4fdeea3fc5bf0
ASN <amansinghnishad0808@gmail.com>
2025-05-04 10:17:45 +0530

    fix

--- src/Components/HeroPage/HeroPage.jsx
+++ src/Components/HeroPage/HeroPage.jsx
@@ -67,7 +67,7 @@ const HeroPage = () => {
           <button
             className="resumeButton"
             onClick={() => {
-              window.open("./public/resume.pdf", "_blank");
+              window.open("/resume.pdf", "_blank");
             }}
           >
             View Resume
