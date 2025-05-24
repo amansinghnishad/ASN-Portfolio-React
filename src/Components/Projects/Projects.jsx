ad3af2196d545284bf82ddaaf385538624fefdca
ASN <amansinghnishad0808@gmail.com>
2025-05-24 16:08:39 +0530

    project changes

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -7,31 +7,31 @@ import "./Projects.css";
 const projects = [
   {
     img: HMS,
-    type: "On Progress",
+    type: "Featured Project",
     title: "Hostel Management System",
-    TryNowlink: "https://github.com/amansinghnishad/Project-HMS",
+    TryNowlink: "https://project-hms-frontend-l3vz.onrender.com",
     codeLink: "https://github.com/amansinghnishad/Project-HMS",
     description:
       "A Hostel Management System (HMS) is a web application designed to streamline the management of hostel facilities. It provides features for room allocation, student registration, and payment tracking, ensuring efficient communication between students and hostel management. The system enhances the overall experience for both students and administrators.",
   },
   {
     img: HobiMenia,
-    type: "On Progress",
+    type: "Featured Project",
     title: "HobiMenia",
     TryNowlink: "https://hobimenia.vercel.app",
     codeLink: "https://github.com/amansinghnishad/Hobimenia",
     description:
       "HobiMenia is a dynamic social media app that connects people through shared interests, enabling users to build communities, post pictures, and write blogs. Its standout AI-powered feature helps craft engaging blogs and suggests trending hashtags for maximum reach. With a seamless interface, users can connect, share, and inspire effortlessly.",
   },
-  {
-    img: CorsaleImageGallery,
-    type: "Featured Project",
-    title: "Corsale Image Gallery",
-    TryNowlink: "https://amansinghnishad.github.io/Corsale-Image-Gallery/",
-    codeLink: "https://github.com/amansinghnishad/Corsale-Image-Gallery",
-    description:
-      "Corsale Image Gallery is a web application that allows users to upload and share images. It features a user-friendly interface, image categorization, and the ability to like and comment on images. The gallery is designed for easy navigation and provides a platform for users to showcase their photography skills.",
-  },
+  // {
+  //   img: CorsaleImageGallery,
+  //   type: "Featured Project",
+  //   title: "Corsale Image Gallery",
+  //   TryNowlink: "https://amansinghnishad.github.io/Corsale-Image-Gallery/",
+  //   codeLink: "https://github.com/amansinghnishad/Corsale-Image-Gallery",
+  //   description:
+  //     "Corsale Image Gallery is a web application that allows users to upload and share images. It features a user-friendly interface, image categorization, and the ability to like and comment on images. The gallery is designed for easy navigation and provides a platform for users to showcase their photography skills.",
+  // },
 ];
 
 const Project = () => {
